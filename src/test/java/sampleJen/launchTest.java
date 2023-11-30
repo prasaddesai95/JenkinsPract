561a26c5cd2151594bf191c3e9c9b8e753648b0c
prasaddesai95 <prasaddesai811@gmail.com>
2023-11-30 18:04:31 +0530

    last

--- src/test/java/sampleJen/launchTest.java
+++ src/test/java/sampleJen/launchTest.java
@@ -1,19 +1,15 @@
 package sampleJen;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
 public class launchTest {
 
-	WebDriver driver;
-	
-	@Test
-	public void m1Test()
-	{
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.in/");
-		System.out.println("Browser launched sucess");
-		
-	}
+//	WebDriver driver;
+//	
+//	@Test
+//	public void m1Test()
+//	{
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("https://www.amazon.in/");
+//		System.out.println("Browser launched sucess");
+//		
+//	}
 }
