bef5c64f01a87fa10f4cb16ea00c741620e829be
prasaddesai95 <prasaddesai811@gmail.com>
2023-11-30 12:52:54 +0530

    hi1

--- src/test/java/sampleJen/launchTest.java
+++ src/test/java/sampleJen/launchTest.java
@@ -1,5 +1,17 @@
 package sampleJen;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class launchTest {
 
+	WebDriver driver;
+	
+	@Test
+	public void m1Test()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("");
+	}
 }
